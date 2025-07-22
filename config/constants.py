77b62e11dd11cf04be2ca8a1f9d19a3ffb502719
Lednir <valsubaan@yahoo.com>
2025-07-22 18:48:59 +0800

    Add throttle delay constant and liveagent mechanigo system user ID

--- config/constants.py
+++ config/constants.py
@@ -4,3 +4,5 @@ Static, non-sensitive constants used throughout the application.
 APP_VERSION = "v1"
 BASE_URL = "https://mechanigo.ladesk.com/api/v3"
 THROTTLE_DELAY = 0.4
+
+LIVEAGENT_MGO_SYSTEM_USER_ID = "system00"
