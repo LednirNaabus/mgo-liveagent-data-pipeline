8625caae32997e305ea8cc353ac2b44cfef149fe
Lednir <valsubaan@yahoo.com>
2025-07-25 20:43:05 +0800

    Change throttle_delay to 0 (temporary), add timezone configuration

--- config/config.py
+++ config/config.py
@@ -2,9 +2,11 @@
 Main configuration file for the project.
 """
 from dotenv import load_dotenv
+import pytz
 import os
 
 load_dotenv()
 
 # TO DO: Add API key validation
 LIVEAGENT_API_KEY = os.getenv('LIVEAGENT_API_KEY')
+MNL_TZ = pytz.timezone("Asia/Manila")
