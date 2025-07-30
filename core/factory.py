6a65583b40d3a0ed740b005bb62ce9b8baaf9cc3
Val Subaan <valsubaan@yahoo.com>
2025-07-30 13:07:06 +0800

    Remove unused imports and comments

--- core/factory.py
+++ core/factory.py
@@ -1,10 +1,7 @@
 from core.extract.Extractor import Extractor
 from config.config import LIVEAGENT_API_KEY
-from fastapi import Request
 import aiohttp
 
-# def create_extractor(max_page: int, per_page: int, table_name: str, session: aiohttp.ClientSession) -> Extractor:
-#     return Extractor(LIVEAGENT_API_KEY, max_page, per_page, table_name, session)
 def create_extractor(
     max_page: int = None,
     per_page: int = None,
