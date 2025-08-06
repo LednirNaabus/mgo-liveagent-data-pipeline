931fbfe0ea73aa1a3f2f0931a0ec82c519723422
Val Subaan <valsubaan@yahoo.com>
2025-08-06 09:02:40 +0800

    Add helper function for extracting datetime on a 6 hour interval

--- utils/date_utils.py
+++ utils/date_utils.py
@@ -1,3 +1,4 @@
+from typing import Tuple
 import pandas as pd
 
 def set_timezone(df: pd.DataFrame, *cols: str, target_tz: str) -> pd.DataFrame:
@@ -9,3 +10,12 @@ def set_timezone(df: pd.DataFrame, *cols: str, target_tz: str) -> pd.DataFrame:
             df[col] = df[col].dt.tz_convert("UTC")
         df[col] = df[col].dt.tz_convert(target_tz).dt.tz_localize(None)
     return df
+
+def get_start_end_str(date: pd.Timestamp) -> Tuple:
+    """Calculate the time with a 6 hour interval."""
+    date = date - pd.Timedelta(hours=6)
+    start = date.floor('h')
+    end = start + pd.Timedelta(hours=6) - pd.Timedelta(seconds=1)
+    start_str = start.strftime("%Y-%m-%d %H:%M:%S")
+    end_str = end.strftime("%Y-%m-%d %H:%M:%S")
+    return start_str, end_str
