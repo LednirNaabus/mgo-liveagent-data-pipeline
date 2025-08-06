61a130c552c72bcf2a87c37051ac16c4574f87c6
Val Subaan <valsubaan@yahoo.com>
2025-08-06 14:19:26 +0800

    Add df util: drop columns function

--- utils/df_utils.py
+++ utils/df_utils.py
@@ -1,4 +1,10 @@
 import pandas as pd
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 def fill_nan_values(df: pd.DataFrame) -> pd.DataFrame:
     for col in df.columns:
@@ -6,4 +12,14 @@ def fill_nan_values(df: pd.DataFrame) -> pd.DataFrame:
             df[col].fillna("", inplace=True)
         else:
             df[col].fillna(0, inplace=True)
+    return df
+
+def drop_cols(df: pd.DataFrame, *cols: str) -> pd.DataFrame:
+    try:
+        existing = [col for col in cols if col in df.columns]
+        if existing:
+            df.drop(columns=existing, inplace=True)
+    except Exception as e:
+        logging.warning(f"Current columns: {df.columns.tolist()}")
+        logging.error(f"Exception occurred while dropping columns: {e}")
     return df
