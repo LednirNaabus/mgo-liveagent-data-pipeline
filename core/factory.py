fe91ade61b94f95c2b3af7012764427afcc9db93
Val Subaan <valsubaan@yahoo.com>
2025-08-06 10:42:46 +0800

    Remove table_name parameter in Extractor constructor

--- core/factory.py
+++ core/factory.py
@@ -5,7 +5,6 @@ import aiohttp
 def create_extractor(
     max_page: int = None,
     per_page: int = None,
-    table_name: str = None,
     session: aiohttp.ClientSession = None,
 ) -> Extractor:
-    return Extractor(LIVEAGENT_API_KEY, max_page, per_page, table_name, session)
+    return Extractor(LIVEAGENT_API_KEY, max_page, per_page, session)
