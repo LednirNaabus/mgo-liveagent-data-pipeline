1e6809563af353c86a74f834179785c402656902
Val Subaan <valsubaan@yahoo.com>
2025-08-07 15:37:25 +0800

    Change dataset to 'conversations'

--- config/constants.py
+++ config/constants.py
@@ -10,7 +10,7 @@ LIVEAGENT_MGO_SPECIAL_USER_ID = "00054iwg"
 
 # BigQuery
 PROJECT_ID = "mechanigo-liveagent"
-DATASET_NAME = "conversations_test"
+DATASET_NAME = "conversations"
 
 MAX_VALUE = 100
 MAX_CONCURRENT_REQUESTS = 15
